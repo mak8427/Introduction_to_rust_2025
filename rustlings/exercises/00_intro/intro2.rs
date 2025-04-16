630c6012d72c2c5df90df329285f8342deff890f
Davide <davide.mattioli.2001@gmail.com>
2025-04-16 17:39:31 +0200

    first exercises

--- rustlings/exercises/00_intro/intro2.rs
+++ rustlings/exercises/00_intro/intro2.rs
@@ -1,5 +1,11 @@
 fn main() {
     // Fix the code to print "Hello world!".
+     
+    let x : i16 = 1000;
+    println!("{}", x );
+
+
     println!("Hello world!");
+
 
 }
--- rustlings/exercises/01_variables/variables6.rs
+++ rustlings/exercises/01_variables/variables6.rs
@@ -1,5 +1,5 @@
 // TODO: Change the line below to fix the compiler error.
-const NUMBER = 3;
+static NUMBER: i64 = 3;
 
 fn main() {
     println!("Number: {NUMBER}");
