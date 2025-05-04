a0199b2d4aaa7e3c9992dd516567a016d7df50f8
Davide <davide.mattioli.2001@gmail.com>
2025-05-04 11:35:10 +0200

    primitives

--- rustlings/exercises/04_primitive_types/primitive_types3.rs
+++ rustlings/exercises/04_primitive_types/primitive_types3.rs
@@ -2,6 +2,8 @@ fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
     // let a = ???
 
+    let mut a: [i8; 300] = [1; 300];
+
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
